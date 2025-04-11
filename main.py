75fa869a16f5f165561be143f15ec9d40d1d89fa
mdatickhassan <mdatickhassansustian72@gmail.com>
2025-04-11 09:28:51 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+my_name = 'Md Atick Hassan'
+age = 21
+is_student = True
+isnot_student= False
+print(type(my_name))
+print(type(age))
+print(type(is_student))
+print(type(isnot_student))
+
+my_age = 21
+any_number = 3
+print(my_age + any_number)
+print(my_age - any_number)
+print(my_age * any_number)
+print(my_age / any_number)
+print(my_age // any_number)
+print(my_age ** any_number)
+
+is_age = 18
+my_age = 21
+print(is_age == my_age)
+print(is_age != my_age)
+print(is_age < my_age)
+print(is_age > my_age)
+print(is_age <= my_age)
+print(is_age >= my_age)
+
+condition1 = 21
+condition2 = 30
+print(condition1 > 15 and  condition2 >21)
+print(condition1 < 15 and  condition2 >21)
+print(condition1 < 15 and  condition2 <21)
+
+print(condition1 < 15 or  condition2 <21)
+print(condition1 < 15 or  condition2 <21)
+print(condition1 < 15 or  condition2 <21)
+
+print (not(condition1 < 13 and  condition2 <21))
+print (not(condition1 > 123 and  condition2 <21))
+print (not(condition1 < 17 and  condition2 <21))
+
+x = 10
+x+=7
+print(x)
+x-=4
+print(x)
+x*=4
+print(x)
+x/=4
+print(x)
+
+s = [1,2,3]
+d = [4,7,9]
+print(s is d)
+print( s is not d)
+
+players = ['atick', 'sabeet', 'arif', 'nonacademy']
+print('atick' not in players)
+print('atick' in players)
+print('rahim' not in players)
+
+
